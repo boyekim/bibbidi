f4e4e7e090d27d2acef6eb60712a83647e43ca35
Sumin <new.sumyang@gmail.com>
2026-07-10 00:32:08 +0900

    first commit

---
+++ src/main/java/com/bibbidi/BibbidiApplication.java
@@ -0,0 +1,13 @@
+package com.bibbidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibbidiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibbidiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bibbidi/BibbidiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibbidi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibbidiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
